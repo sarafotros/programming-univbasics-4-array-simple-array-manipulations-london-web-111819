a3876b646aa5c291f8f603d36d9fb34bfc8eeec2
Sara Fotros <sarafotros1@yahoo.com>
2019-11-10 16:09:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -36,9 +36,10 @@ def using_concat(my_favorite_things,more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-def using_insert()
-
-
+def using_insert(list_of_programming_languages,another_language)
+  list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+   another_language = "Python"
+ 
 
 
   
